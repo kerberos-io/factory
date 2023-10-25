6678b1cc8ae2e81f2248ecd92be825aacf7255bf
Cedric Verstraeten <cedric@verstraeten.io>
2023-10-25 17:12:29 +0200

    add white-labeling option Kerberos Factory

---
+++ kubernetes/kerberos-factory/assets/env.js
@@ -0,0 +1,6 @@
+(function(window) {
+  window["env"] = window["env"] || {};
+  // Environment variables
+  window["env"]["environment"] = "whitelabel";
+  window["env"]["pageTitle"] = "Your application";
+})(this);
